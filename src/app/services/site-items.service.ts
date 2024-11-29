027346b8e01d9841b764933f1b241880e9e6b89d
x-deepact <junminghong930@gmail.com>
2024-11-28 22:40:27 -0800

    datafetch

---
+++ src/app/services/site-items.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SiteItemsService {
+  private apiUrl = 'http://localhost:8080/siteItem'; // Backend API URL
+
+  constructor(private http: HttpClient) {}
+
+  getsiteItems(pageId: number, pageSize: number): Observable<any> {
+    const url = `${this.apiUrl}?page_id=${pageId}&page_size=${pageSize}`;
+    return this.http.get<any>(url);
+  }
+}
